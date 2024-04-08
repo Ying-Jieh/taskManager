e5ffb88209fee7c25182b6738568a27c3f76f050
YingJiehXia <yix050@ucsd.edu>
2024-04-08 15:13:38 -0700

    add routes and controllers folders

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 This is a task manager web application using express and mongodb
 
+
+## Files
+- `app.js`: The main js file, managing the url and middlewares
+- `routes/tasks.js`: Use the route functions in controllers, setup the routes
+- `controllers/tasks.js`: Implement the route functions
--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks');
+
+// routes
+app.get('/', (req, res) => {
+    res.send('Task Manager App')
+});
+
+app.use('/api/v1/tasks', tasks);
+
+const port = 3000;
+
+app.listen(port, console.log(`Serve is listening on port ${port}...`));
